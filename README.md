4b72e2f8f8b365fb8f4cdca9f05d7c2b45600727
Daniel Speichert <daniel@speichert.pro>
2014-03-18 01:11:07 +0100

    Init test

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 otshosting-provisioning
 =======================
+
+
+```bash
+#!/bin/bash -ex
+
+apt-get install -y -q python-paramiko python-yaml python-jinja2 python-simplejson
+apt-get install -y -q git-core
+apt-get install -y -q software-properties-common python-software-properties
+
+apt-add-repository ppa:rquillo/ansible
+apt-get update
+apt-get install ansible
+
+echo "localhost" > ~/ansible_hosts
+export ANSIBLE_HOSTS=~/ansible_hosts
+
+ansible-pull -U https://github.com/DSpeichert/otshosting-provisioning.git -d /srv 
+
+exit 0
+```
---
+++ templates/config.php
@@ -0,0 +1,14 @@
+<?php
+// AT MINIMUM UNCOMMENT AND CONFIGURE ONE OF THE FOLLOWING OPTIONS:
+
+// OPTION ONE: READ MYSQL CONFIG FROM config.lua - RECOMMENDED
+define('TFS_CONFIG', '/home/otsmanager/otserver/config.lua');
+
+// OPTION TWO: SPECIFY MYSQL CONNECTION DETAILS HERE
+// define('TFS_CONFIG', array('mysqlHost' => '127.0.0.1', 'mysqlDatabase' => 'tfs', 'mysqlUser' => 'tfs', 'mysqlPass' => 'tfs'));
+// THIS OPTION IS DISCOURAGED AS SOME CODE MIGHT DEPEND ON OTHER VALUES FROM TFS CONFIG
+
+// IF YOU WANT TO CHANGE SOMETHING ELSE, BE SMART AND FOLLOW THE PATTERN
+defined('TFS_ROOT') or define('TFS_ROOT', '/home/otsmanager/otserver');
+define('CORS_ALLOW_ORIGIN', '*');
+define('ENABLE_DEBUG', true);
