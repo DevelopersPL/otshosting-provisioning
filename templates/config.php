603ce497e5b2ca745b43269cae693f59d29a4d65
Daniel Speichert <daniel@speichert.pl>
2014-07-18 19:41:26 -0400

    Sample config changed

--- templates/config.php
+++ templates/config.php
@@ -1,14 +1,29 @@
 <?php
-// AT MINIMUM UNCOMMENT AND CONFIGURE ONE OF THE FOLLOWING OPTIONS:
+/* AT MINIMUM UNCOMMENT AND CONFIGURE PATH TO TFS - this is where config.lua is located
+   examples:
+     - /home/otsmanager/forgottenserver
+     - C:\Users\OTS\forgottenserver
+*/
+define('TFS_ROOT', '/home/otsmanager/forgottenserver');
+// ^^^^^^^ THIS IS VERY IMPORTANT ^^^^^^^
 
-// OPTION ONE: READ MYSQL CONFIG FROM config.lua - RECOMMENDED
-define('TFS_CONFIG', '/home/otsmanager/forgottenserver/config.lua');
+// ------------ OPTIONAL SETTINGS -------------
+// define('ALLOWED_VOCATIONS', serialize(array(0))); // if you want everyone to start with no vocation (RL style)
+// define('ALLOWED_VOCATIONS', serialize(array(1, 2, 3, 4))); // default
+// define('NEW_PLAYER_LEVEL', 8);
+// define('NEW_PLAYER_TOWN_ID', 1);
 
-// OPTION TWO: SPECIFY MYSQL CONNECTION DETAILS HERE
-// define('TFS_CONFIG', array('mysqlHost' => '127.0.0.1', 'mysqlDatabase' => 'tfs', 'mysqlUser' => 'tfs', 'mysqlPass' => 'tfs'));
-// THIS OPTION IS DISCOURAGED AS SOME CODE MIGHT DEPEND ON OTHER VALUES FROM TFS CONFIG
+// define('HOUSES_AUCTIONS', true); // enable house auction system?
+// define('HOUSES_AUCTION_TIME', 'P7D'); // DateInterval spec notation: http://www.php.net/manual/en/dateinterval.construct.php
+// define('HOUSES_PER_PLAYER', 1);
+// define('HOUSES_PER_ACCOUNT', 1);
 
-// IF YOU WANT TO CHANGE SOMETHING ELSE, BE SMART AND FOLLOW THE PATTERN
-defined('TFS_ROOT') or define('TFS_ROOT', '/home/otsmanager/forgottenserver');
-define('CORS_ALLOW_ORIGIN', '*');
-define('ENABLE_DEBUG', false);
+// define('HOUSES_AUCTIONS', true); // enable house auction system?
+// define('HOUSES_AUCTION_TIME', 'P7D'); // DateInterval spec notation: http://www.php.net/manual/en/dateinterval.construct.php
+// define('HOUSES_PER_PLAYER', 1);
+// define('HOUSES_PER_ACCOUNT', 1);
+
+// define('DISABLE_PLUGINS', false);
+// define('ENABLED_PLUGINS', serialize(array('ratelimiter.php', 'simple.php', 'ipban.php')));
+
+// IF YOU WANT TO CHANGE SOMETHING ELSE, BE SMART AND FOLLOW THE PATTERN, CHECK index.php!
